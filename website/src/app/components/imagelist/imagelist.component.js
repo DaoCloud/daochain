dd74a9cab1bc48809c88f0621bb3e20942f7e461
congjiujiu <nighting.cong@daocloud.io>
2016-11-11 04:27:04 +0800

    add sign and verify function

--- website/src/app/components/imagelist/imagelist.component.js
+++ website/src/app/components/imagelist/imagelist.component.js
@@ -1,4 +1,5 @@
 import template from './imagelist.html';
+import dialogTemplate from './dialog.html';
 import $ from 'jquery';
 // import controller from './imagelist.controller';
 import './imagelist.scss';
@@ -14,17 +15,19 @@ function Inject(...dependencies) {
     };
 }
 
-@Inject('$scope')
+@Inject('$scope', '$daoDialog')
 class controller {
-  constructor(scope) {
+  constructor(scope, $daoDialog) {
     // this.dataA = 'sss';
     this.scope = scope;
+    this.$daoDialog = $daoDialog;
   }
 
-  $onInit(){
+  $onInit() {
     this.appendTo = document.querySelector('.images');
     this.name = 'imagelist';
     this.data = [];
+    this.val = 0;
 
     $.ajax({
       type: "GET",
@@ -51,6 +54,52 @@ class controller {
         });
       }
     });
+
+    this.runProgress = (index) => {
+      // $('.title')[index].style.display = "none";
+      $('.pull-progress')[index].style.display = "block";
+
+      let startProgess = setInterval(() => {
+        if (this.val > 0.8) {
+          clearInterval(startProgess);
+          console.log("cleare");
+        }
+        let random_increment = Math.random(1) / 10;
+        this.scope.$apply(() => {
+          this.val += random_increment;
+        });
+      }, 200);
+    }
+
+    this.pullImage = (image, index) => {
+      this.runProgress(index);
+      const postData = {
+        "repo_tag": image
+      };
+      $.ajax({
+        type: "POST",
+        url: "http://10.1.4.173:8000/api/pull-image",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        data: JSON.stringify(postData),
+        success: res => {
+          this.scope.$apply(() => {
+            this.val = 1;
+          });
+          setTimeout(() => {
+            this.openDialog();
+            $('.pull-progress')[index].style.display = "none";
+          }, 100);
+        }
+      });
+    }
+
+    this.openDialog = () => {
+      this.$daoDialog.open({
+        template: dialogTemplate
+      });
+    }
   }
 }
 
