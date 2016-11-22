29f61385b3854f9bb63b3c133cacb15d6d059e79
revol.cai <revol.cai@daocloud.io>
2016-11-22 20:14:10 +0800

    add geth
    Signed-off-by: revol.cai <revol.cai@daocloud.io>

---
+++ geth/monitored-geth-client/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+cd /root/eth-net-intelligence-api
+sed -ie "s/HOSTNAME_ENV/$(hostname)/g" app.json
+sed -ie "s/WS_SERVER_ENV/$(echo $WS_SERVER|sed -e 's/[]\/$*.^|[]/\\&/g')/g" app.json
+sed -ie "s/WS_SECRET_ENV/$(echo $WS_SECRET|sed -e 's/[]\/$*.^|[]/\\&/g')/g" app.json
+/usr/bin/pm2 start ./app.json
+sleep 3
+
+/usr/bin/geth --datadir "/root/.ethereum" init /root/files/genesis.json
+
+/usr/bin/geth --networkid=23333 --datadir "/root/.ethereum" \
+--rpc --rpccorsdomain="*" --rpcaddr '0.0.0.0' --rpcapi "admin,db,personal,eth,net,web3,miner" \
+--bootnodes="$NODES"
