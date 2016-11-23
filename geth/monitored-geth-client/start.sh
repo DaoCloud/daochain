0b53d43f89856118000eaa08e85e7ba56c94b9b4
revol.cai <revol.cai@daocloud.io>
2016-11-23 21:32:34 +0800

    alpine dont have bash
    Signed-off-by: revol.cai <revol.cai@daocloud.io>

--- geth/monitored-geth-client/start.sh
+++ geth/monitored-geth-client/start.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 cd /root/eth-net-intelligence-api
 sed -ie "s/HOSTNAME_ENV/$(hostname)/g" app.json
 sed -ie "s/WS_SERVER_ENV/$(echo $WS_SERVER|sed -e 's/[]\/$*.^|[]/\\&/g')/g" app.json
 sed -ie "s/WS_SECRET_ENV/$(echo $WS_SECRET|sed -e 's/[]\/$*.^|[]/\\&/g')/g" app.json
-/usr/bin/pm2 start ./app.json
+/usr/local/bin/pm2 start ./app.json
 sleep 3
 
 /usr/bin/geth --datadir "/root/.ethereum" init /root/files/genesis.json
