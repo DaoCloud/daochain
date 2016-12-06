0212887113d1accb3f54b88be79307977689b0c5
revol.cai <revol.cai@daocloud.io>
2016-12-06 11:38:54 +0800

    add public ip
    Signed-off-by: revol.cai <revol.cai@daocloud.io>

--- geth/monitored-geth-client/start.sh
+++ geth/monitored-geth-client/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 cd /root/eth-net-intelligence-api
-sed -ie "s/HOSTNAME_ENV/$(hostname)/g" app.json
+# sed -ie "s/HOSTNAME_ENV/$(hostname)/g" app.json
+sed -ie "s/HOSTNAME_ENV/$(hostname)-$(node -e "require('http').get('http://ip.taobao.com/service/getIpInfo.php?ip=myip',(res)=>{res.setEncoding('utf8');data='';res.on('data',(chunk)=>data+=chunk);res.on('end',()=>{console.log(JSON.parse(data).data.ip)})})")/g" app.json
 sed -ie "s/WS_SERVER_ENV/$(echo $WS_SERVER|sed -e 's/[]\/$*.^|[]/\\&/g')/g" app.json
 sed -ie "s/WS_SECRET_ENV/$(echo $WS_SECRET|sed -e 's/[]\/$*.^|[]/\\&/g')/g" app.json
 /usr/local/bin/pm2 start ./app.json
